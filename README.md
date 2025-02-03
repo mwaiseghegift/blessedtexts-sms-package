68c3636737dc93756afa935abbbf3e5733d9f1f5
mwaiseghegift <itsregalo047@gmail.com>
2025-02-03 19:06:57 +0300

    FEAT: Update .gitignore, README, requirements, and setup for project enhancements

--- README.md
+++ README.md
@@ -29,5 +29,3 @@ print(balance)
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
-
-
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="blessedtexts_sms",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Gift Mwaiseghe",
+    author_email="mwaiseghe.dev@gmail.com",
     description="A Python client for the BlessedTexts SMS API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/blessedtexts_sms",
+    url="https://github.com/mwaiseghegift/blessedtexts-sms-package",
     packages=find_packages(),
     install_requires=[
         "requests"
